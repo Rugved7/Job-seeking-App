8c4720e8cffca4c673fb587d6165d09f358305c9
Rugved7 <rugvedagasti7@gmail.com>
2024-04-30 18:32:59 +0530

    Changes updated and Resumed with Client side work

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,15 +1,32 @@
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import { Context } from "./main";
+import {
+  BrowserRouter as Routes,
+  Route,
+  Router,
+} from "react-router-dom";
+import Application from "./Components/Application/Application";
+import Home from "./Components/Home/Home";
+import Footer from "./Components/Layout/Footer";
+import Navbar from "./Components/Layout/Navbar";
+import Login from "./Components/Auth/Login";
+import Register from "./Components/Auth/Register";
 
-function App() {
+const App = () => {
   return (
     <>
-      <div>
-        Hello
-      </div>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+
+        </Routes>
+        <Footer />
+      </Router>
     </>
   );
-}
+};
 
 export default App;
